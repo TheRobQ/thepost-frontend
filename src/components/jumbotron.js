7f3040f561df6eeabab80af25780675cb70e1b1d
Robert Quan <rob.quan@gmail.com>
2018-03-13 20:34:24 -0600

    Added loading screen

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import LargePics from './largePics'
-import{Grid, Row, Column} from 'react-bootstrap'
+import {Grid, Row, Column} from 'react-bootstrap'
 
-const Jumbotron = ({trending_articles, getArticleId }) => {
+const Jumbotron = ({trending_articles, getArticleId}) => {
   return (
-      <div className="row topRow" style={{backgroundColor:'black'}}>
-        {trending_articles.map(article=> (<LargePics key={article.id} article={article} getArticleId={getArticleId}/>))}
-      </div>
-  )
+    trending_articles.length === 0 ?
+     <div>Please wait while we fetch your content</div> :
+     <div className="row topRow" style={{
+        backgroundColor: 'black'
+     }}>
+      {trending_articles.map(article => (<LargePics key={article.id} article={article} getArticleId={getArticleId}/>))}
+    </div>)
 }
 export default Jumbotron
