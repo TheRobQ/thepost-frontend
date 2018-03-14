aa0fa8b6bb3b0f30c69d2fe89f0fe22e8cfe78e9
Robert Quan <rob.quan@gmail.com>
2018-03-13 20:53:48 -0600

    load fixed

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -3,13 +3,18 @@ import LargePics from './largePics'
 import {Grid, Row, Column} from 'react-bootstrap'
 
 const Jumbotron = ({trending_articles, getArticleId}) => {
+  console.log(trending_articles.length);
+  if(trending_articles.length === 0){
   return (
-    trending_articles.length === 0 ?
-     <div>Please wait while we fetch your content</div> :
      <div className="row topRow" style={{
         backgroundColor: 'black'
+     }}><p style={{color:'white', marginTop: '48px', fontSize:'50px'}}>Please wait while we fetch your content</p></div>)
+   } else{
+     return (<div className="row topRow" style={{
+        backgroundColor: 'black'
      }}>
-      {trending_articles.map(article => (<LargePics key={article.id} article={article} getArticleId={getArticleId}/>))}
-    </div>)
+       {trending_articles.map(article => (<LargePics key={article.id} article={article} getArticleId={getArticleId}/>))}
+     </div>)
+   }
 }
 export default Jumbotron
